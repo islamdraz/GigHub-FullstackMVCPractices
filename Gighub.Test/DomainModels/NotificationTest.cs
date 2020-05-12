405f8a152be005a44c5eedd5fe88e18f4cfddafe
islam.draz <eng.islamdraz@gmail.com>
2020-05-12 12:55:18 +0200

    Complete Model unit tests and do refactoring in Applicationuser model

---
+++ Gighub.Test/DomainModels/NotificationTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using FullStackCourse1.Core.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Gighub.Test.DomainModels
+{
+    [TestClass]
+public    class NotificationTest
+    {
+
+        [TestMethod]
+        public void GigUpdate_WhenCall_NewNotificationWithUpdateType()
+        {
+            var gig=new Gig();
+            
+            var notification = Notification.GigUpdated(gig, DateTime.Now, "test");
+
+            notification.Type.Should().Be(NotificationType.Update);
+            notification.Gig.Should().Be(gig);
+        }
+
+        [TestMethod]
+        public void GigCancel_WhenCall_NewNotificationWithCancelType()
+        {
+            var gig = new Gig();
+
+            var notification = Notification.GigCancel(gig);
+
+            notification.Type.Should().Be(NotificationType.Cancel);
+            notification.Gig.Should().Be(gig);
+        }
+    }
+}
